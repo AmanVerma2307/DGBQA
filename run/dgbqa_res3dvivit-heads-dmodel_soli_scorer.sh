2ec3f702a1ca8003cb1599351f31d37ca65368b2
AmanVerma2307 <eez227536@ee.iitd.ac.in>
2025-06-22 23:00:37 +0530

    experiments added

---
+++ run/dgbqa_res3dvivit-heads-dmodel_soli_scorer.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 1
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-64_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-64_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-64_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 1
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-128_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-128_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-128_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 1
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-4heads-256_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-8heads-256_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-0_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-pt005_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-pt05_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-1pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
+python './scorer.py' --exp_name dgbqa_res3dvivit-16heads-256_2-2pt5_soli --filePath dgbqa_res3dvivit-heads-dmodel_soli --init 0
